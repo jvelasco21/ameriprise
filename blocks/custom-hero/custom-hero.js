f611d2c6424f3b9055d6aafa3c943de88ba56f70
John Velasco <C=John.Velasco@charter.com>
2024-03-07 12:50:47 -0500

    Custom Hero Login

--- blocks/custom-hero/custom-hero.js
+++ blocks/custom-hero/custom-hero.js
@@ -1,8 +1,14 @@
 export default function decorate(block) {
+    console.log(block);
     const heroImg = block.querySelector('picture');
     const heroBg = document.createElement('div');
+    const heroContent = block.querySelector('div:not(.custom-hero-image)');
+    const loginForm = block.closest('main').querySelector('.login-form')
+
     heroImg.parentElement.remove();
     heroBg.append(heroImg);
     heroBg.className = 'custom-hero-image';
     block.prepend(heroBg);
+    heroContent.className = "custom-hero-content";
+    heroContent.append(loginForm);
 }
